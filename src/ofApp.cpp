89db63b2c316e271cd9382504c044d0935bc585b
Angel Gamis <agamis@rrc.ca>
2024-10-30 12:34:19 -0500

    Reimplementing ofx gui code. Allowing grid to reset and generate a new grid when clicking restart

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,44 +3,41 @@
 #include "box.h"
 #include "grid.h"
 
-Box box1;
-Box box2;
 Grid mainGrid;
+float boxSize = 20.0f;
 
 //--------------------------------------------------------------
 void ofApp::setup() {
 	// Setting up gui
-	//gui.setup();
-	//gui.add(gridColumns.setup("# of Columns", 8, 5, 50));
-	//gui.add(gridRows.setup("# of Rows", 8, 5, 50));
-	//gui.add(numberOfMines.setup("# of Mines", 10, 2, 450));
-	//gui.add(restartBtn.setup("Submit"));
+	gui.setup();
+	gui.add(gridColumns.setup("# of Columns", 8, 5, 50));
+	gui.add(gridRows.setup("# of Rows", 8, 5, 50));
+	gui.add(numberOfMines.setup("# of Mines", 10, 2, 450));
+	gui.add(restartBtn.setup("Restart"));
 
 	//gui.add(flaggingMode.setup("Flagging Mode", false));
 
-	//ofSetupOpenGL(100, 100, OF_WINDOW);
 	ofBackground(200, 200, 200);
 
 	glm::vec2 box1position(0, 0);
 	glm::vec2 box2position(10, 0);
 
-	//box1 = Box(glm::vec2(0, 0), 10.0f, false, 0);
-	//box2 = Box(glm::vec2(10, 0), 10.0f, false, 0);
-
-	mainGrid = Grid(8, 8, 10, 20.0f);
+	mainGrid = Grid(8, 8, 10, boxSize);
 	mainGrid.generateGrid();
 }
 
 //--------------------------------------------------------------
 void ofApp::update() {
-
+	if (restartBtn) {
+		mainGrid.generateGrid(gridRows, gridColumns, numberOfMines, boxSize);
+	}
 }
 
 //--------------------------------------------------------------
 void ofApp::draw() {
-	//box1.display();
-	//box2.display();
 	mainGrid.displayGrid();
+
+	gui.draw();
 }
 
 //--------------------------------------------------------------
